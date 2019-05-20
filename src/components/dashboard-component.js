4dd7e8bf1280cdeb9c17f1f9193125e5618c760c
Alejandro Barata <alexbgdev@gmail.com>
2019-05-21 00:19:35 +0200

    init dashboard

---
+++ src/components/dashboard-component.js
@@ -0,0 +1,12 @@
+'use strict'
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class DashboardComponent extends React.Component{
+  render(){
+    return (
+      <h1>Description Jira component</h1>
+    )
+  }
+}
